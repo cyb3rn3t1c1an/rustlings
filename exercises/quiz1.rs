0aa4e543cbd7b88475dc7c7e70bbcf683a2b59c2
cyb3rn3t1c1an <2145095+cyb3rn3t1c1an@users.noreply.github.com>
2024-05-26 21:07:45 -0400

    Add solutions for: intro, variables, functions, if and primitive types

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,10 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(amount: u32) -> u32 {
+    if amount > 40 { amount * 1 } else { amount * 2 }
+}
 
 // Don't modify this function!
 #[test]
